82ecdc578ae2bf1f3d6293da6765a4e20bf6ca4f
Neet Shah <shahn17@mcmaster.ca>
2018-12-26 03:06:24 -0500

    Update progressBar and Score

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var pickedAns:Bool = false
     var questionNum:Int = 0
     var correctAns:Bool = false
+    var score:Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -25,6 +26,8 @@ class ViewController: UIViewController {
         let firstQuestion = allQuestions.list[questionNum]
         questionLabel.text = firstQuestion.questionText
         correctAns = firstQuestion.answer
+        scoreLabel.text = "Score: 0"
+        progressLabel.text = "1 / 13"
     }
 
 
@@ -39,19 +42,24 @@ class ViewController: UIViewController {
     
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNum+1) / 13"
+        
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNum + 1)
     }
     
 
     func nextQuestion() {
         questionLabel.text = allQuestions.list[questionNum].questionText
         correctAns = allQuestions.list[questionNum].answer
+        updateUI()
     }
     
     
     func checkAnswer() {
         if pickedAns == correctAns {
             print("Correct")
+            score += 1
         } else {
             print("Incorrect")
         }
@@ -62,6 +70,7 @@ class ViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
             print("Game Over")
             questionNum = 0
+            score = 0
         }
         nextQuestion()
     }
